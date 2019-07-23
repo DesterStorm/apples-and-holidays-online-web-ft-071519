9a22ae81c25da907b1f3df454f76db9b878c4e86
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-23 23:31:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,7 +36,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
     }
   }
   holiday_hash[:winter][:christmas] << "Balloons"
-  
+  holiday_hash[:winter][:new_years] << "Balloons"
 end
 
 
