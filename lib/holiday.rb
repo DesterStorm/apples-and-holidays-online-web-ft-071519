a342512e9914dfb50af73711fe124c6d893efa46
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-23 23:48:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,6 +67,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   count += 1 
   else holiday_hash[:spring][:memorial_day] << "Grill"
 end
+end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
